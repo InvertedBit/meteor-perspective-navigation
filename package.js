2ad352170abe6135d207cb6d62ad31d77c1d17d5
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-08-29 20:15:36 +0200

    order

--- package.js
+++ package.js
@@ -4,9 +4,11 @@ Package.describe({
 
 Package.on_use(function (api) {
 
+    api.add_files("component.css", "client");
+    api.add_files("modernizr.custom.25376.js", "client");
   api.add_files("classie.js", "client");
   api.add_files("menu.js", "client");
-  api.add_files("modernizr.custom.25376.js", "client");
-  api.add_files("component.css", "client");
+
+
 
 });
