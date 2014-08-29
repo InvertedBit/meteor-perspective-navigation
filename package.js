15f0a1e2dbee0ec02989fd290dd37192004346bf
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-08-29 20:11:05 +0200

    my first package

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  "summary": "Perspective page view navigation."
+});
+
+Package.on_use(function (api) {
+
+  api.add_files("classie.js", "client");
+  api.add_files("menu.js", "client");
+  api.add_files("modernizr.custom.25376.js", "client");
+  api.add_files("component.css", "client");
+
+});
