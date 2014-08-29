4033b76a033fe9a1a63021a8b08677d188279e06
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-08-29 20:16:50 +0200

    again

--- package.js
+++ package.js
@@ -6,8 +6,8 @@ Package.on_use(function (api) {
 
     api.add_files("component.css", "client");
     api.add_files("modernizr.custom.25376.js", "client");
-  api.add_files("classie.js", "client");
-  api.add_files("menu.js", "client");
+    api.add_files("classie.js", "client");
+    api.add_files("menu.js", "client");
 
 
 
