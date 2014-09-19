6883c563dc939be13b5c4c8d6971b4a547444e5d
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-09-19 20:26:17 +0200

    19.09.14

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 Package.describe({
-  "summary": "Perspective page view navigation."
+    
+    "summary": "Perspective page view navigation."
+    version: "1.0.0",
+    name: "jelenajjo:meteor-perspective-navigation",
+    git: "https://github.com/jelenajjo/meteor-perspective-navigation.git"
+    
 });
 
 Package.on_use(function (api) {
-
-    api.add_files("component.css", "client");
-    api.add_files("modernizr.custom.25376.js", "client");
-    api.add_files("classie.js", "client");
-    api.add_files("menu.js", "client");
-
-
-
+    
+    api.addFiles("component.css", "client");
+    api.addFiles("modernizr.custom.25376.js", "client");
+    api.addFiles("classie.js", "client");
+    api.addFiles("menu.js", "client");
+    
 });
